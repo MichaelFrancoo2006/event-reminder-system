b880fff9868102cc4956f4e8ee5b1068db2ed7ba
Michael Francoo Fredy <michaelfrancoo4917@gmail.com>
2024-12-23 18:29:44 +0300

    Initial setup - Created the event structure and menu system

---
+++ event_reminder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Event {
+    string name;
+    string date; // Format: "YYYY-MM-DD"
+};
+
+void menu();
+
+int main() {
+    menu();
+    return 0;
+}
+
+void menu() {
+    cout << "Welcome to the Event Reminder System!\n";
+    cout << "Menu will be implemented soon.\n";
+}
